d2ca95cb9fa6c590094fcfe27bb7523df8bb7b8c
Smail-hub <amilsafin@gmail.com>
2026-09-04 12:06:05 +0500

    removed_junk

--- proba_pera.py
+++
@@ -1,9 +0,0 @@
-@app.get("/users")
-async def get_users(
-    user_id: Annotated[int | None, Query()] = None,
-) -> list[User]:
-    async with asyncsessionmaker.begin() as session:
-        stmt = select(User).where(User.isactive.is(True))
-        if user_id is not None:
-            stmt = stmt.where(User.id == user_id)
-        return (await session.scalars(stmt)).all()
