050f66984f3acfeeb6083365c2922f06c4379c27
Smail-hub <amilsafin@gmail.com>
2026-09-17 01:35:27 +0500

    wrapped_the_functions_in_a_loop

--- Password.py
+++ Password.py
@@ -6,13 +6,35 @@ def has_digit(password):
 def is_very_long(password):
 	return len(password) > 12
 
+def has_letters(password):
+	return any (symbol.isalpha() for symbol in password)
+
+def has_upper_letters(password):
+    return any(symbol.isupper() for symbol in password)
+
+def has_lower_letters(password):
+	return any(symbol.islower() for symbol in password)
+
+checks = [
+	has_digit, is_very_long, has_letters,
+	has_upper_letters, has_lower_letters
+]
+
 score = 0
 
-if has_digit(password):
-	score = score + 2
-
-if is_very_long(password):
-	score = score + 2
-
-
-print(f"рейтинг пароля {score}")
+for checks in checks:
+	if checks(password):
+		score += 2
+#if has_digit(password):
+#	score = score + 2
+#
+#if is_very_long(password):
+#	score = score + 2
+#
+#if has_letters(password):
+#	score = score + 2
+#
+#if has_upper_letters(password):
+#	score = score + 2
+#
+print(f"Рейтинг пароля: {score}")
